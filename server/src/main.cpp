4c9bf1539757056270214cac4b0e91cb1b28e3f5
N/A <N@A.invalid>
2025-02-24 13:56:58 -0800

    update

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -35,7 +35,7 @@ main(int argc, char* argv[])
         std::cerr <<
             "Usage: servermain <address> <port> <doc_root> <threads>\n" <<
             "Example:\n" <<
-            "    websocket-chat-server 0.0.0.0 8080 . 5\n";
+            "    servermain 0.0.0.0 8080 . 5\n";
         return EXIT_FAILURE;
     }
     auto address = net::ip::make_address(argv[1]);
